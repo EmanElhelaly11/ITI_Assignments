224d17de16c0f81285a1b033b59cf02d86440ce3
EmanElhelaly <emanelhelaly11@gmail.com>
2023-08-12 01:01:51 +0200

    day1_assignments

---
+++ Week_1/Day_1/Assignment_2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main(){
+int x,y,z;
+
+printf("Please Enter Number 1: ");
+scanf("%d",&x);
+
+printf("Please Enter Number 2: ");
+scanf("%d",&y);
+
+printf("Please Enter Number 3: ");
+scanf("%d",&z);
+
+printf("Number 3: %d\n",z);
+printf("Number 2: %d\n",y);
+printf("Number 1: %d\n",x);
+
+}
---
+++ Week_1/Day_1/Assignment_3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(){
+int a,b;
+
+printf("Please Enter Number a: ");
+scanf("%d",&a);
+
+printf("Please Enter Number b: ");
+scanf("%d",&b);
+
+printf("a + b = %d\n",a+b);
+printf("a - b = %d\n",a-b);
+printf("a & b = %d\n",a&b);
+printf("a | b = %d\n",a|b);
+printf("a ^ b = %d\n",a^b);
+
+}
